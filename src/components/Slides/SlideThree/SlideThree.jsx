ce2fb2c8cf7b690b323bb664dfe88c1cabc77ab6
corey duke <cdukedev@gmail.com>
2023-03-13 09:30:45 -0400

    Added styling to cards in slide 3

--- src/components/Slides/SlideThree/SlideThree.jsx
+++ src/components/Slides/SlideThree/SlideThree.jsx
@@ -9,13 +9,25 @@ const SlideThree = () => {
           <div className="how__left-main-text">
             <h2>Services</h2>
             <p className="how">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat quisquam iusto voluptatem tempora perferendis reiciendis, reprehenderit amet saepe deserunt molestiae quas quaerat quae optio illo praesentium earum delectus animi consectetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio quos unde accusamus facere sequi laboriosam corporis odio, nostrum itaque quod et eum cum incidunt voluptatem amet, nulla error nemo. 
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+              quisquam iusto voluptatem tempora perferendis reiciendis,
+              reprehenderit amet saepe deserunt molestiae quas quaerat quae
+              optio illo praesentium earum delectus animi consectetur? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio
+              quos unde accusamus facere sequi laboriosam corporis odio, nostrum
+              itaque quod et eum cum incidunt voluptatem amet, nulla error nemo.
             </p>
           </div>
           <div className="how__left-main-text">
             <h2>Platform</h2>
             <p className="how">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat quisquam iusto voluptatem tempora perferendis reiciendis, reprehenderit amet saepe deserunt molestiae quas quaerat quae optio illo praesentium earum delectus animi consectetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio quos unde accusamus facere sequi laboriosam corporis odio, nostrum itaque quod et eum cum incidunt voluptatem amet, nulla error nemo. 
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+              quisquam iusto voluptatem tempora perferendis reiciendis,
+              reprehenderit amet saepe deserunt molestiae quas quaerat quae
+              optio illo praesentium earum delectus animi consectetur? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio
+              quos unde accusamus facere sequi laboriosam corporis odio, nostrum
+              itaque quod et eum cum incidunt voluptatem amet, nulla error nemo.
             </p>
           </div>
         </article>
@@ -23,20 +35,26 @@ const SlideThree = () => {
           <div className="how__left-main-text">
             <h2>Partnerships</h2>
             <p className="how">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat quisquam iusto voluptatem tempora perferendis reiciendis, reprehenderit amet saepe deserunt molestiae quas quaerat quae optio illo praesentium earum delectus animi consectetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio quos unde accusamus facere sequi laboriosam corporis odio, nostrum itaque quod et eum cum incidunt voluptatem amet, nulla error nemo. ç
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+              quisquam iusto voluptatem tempora perferendis reiciendis,
+              reprehenderit amet saepe deserunt molestiae quas quaerat quae
+              optio illo praesentium earum delectus animi consectetur? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio
+              quos unde accusamus facere sequi laboriosam corporis odio, nostrum
+              itaque quod et eum cum incidunt voluptatem amet, nulla error nemo.
+              ç
             </p>
           </div>
           <div className="how__left-main-text">
             <h2>Solutions</h2>
-            <pre className="how">
-    The Solution 
-What is your value proposition?
-Screenshot or demo of your PRODUCT/SERVICE or record screen video walking the audience through the solution.
-How many clients do you have?
-What they want and why? Why will they choose you instead of your competition?
-What do you develop based on their feedback?
-Walk me through how it works using a case or explaining the processes involved.
-            </pre>
+            <p className="how">
+              The Solution What is your value proposition? Screenshot or demo of
+              your PRODUCT/SERVICE or record screen video walking the audience
+              through the solution. How many clients do you have? What they want
+              and why? Why will they choose you instead of your competition?
+              What do you develop based on their feedback? Walk me through how
+              it works using a case or explaining the processes involved.
+            </p>
           </div>
         </article>
       </section>
